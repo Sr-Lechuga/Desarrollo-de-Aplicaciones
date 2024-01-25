dc0a20e858ded5a39d1fc1ff90a7d43be2ad3624
Sr.Lechuga <jonattanlima@gmail.com>
2024-01-24 21:36:22 -0300

    Update(Ejercicio_1): Retrieve input information and show it in message

--- Semana 1/Codigo/JS/codigo.js
+++ Semana 1/Codigo/JS/codigo.js
@@ -1,5 +1,10 @@
 document.getElementById("btn_guardar").addEventListener("click",MostrarMensaje);
 
 function MostrarMensaje() {
-    alert("Buenas noches!");
+    let nombre = document.getElementById("txt_nombre").value;
+    let apellido = document.getElementById("txt_apellido").value;
+    let grupo = document.getElementById("slc_grupo").value;
+
+    console.log(`Hola ${nombre} ${apellido}`);
+    console.log(`Bienvenido al grupo de nombre ${grupo}`);
 }
